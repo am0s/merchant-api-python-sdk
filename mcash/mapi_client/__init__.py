9ee79e2cc89259981c52106476ef846c1d6b96cd
Jan Borsodi <jb@aplia.no>
2017-01-12 12:34:59 +0100

    Remove urlfetch from the toplevel import
    Urlfetch requires google appengine so should not be available by
    default.

--- mcash/mapi_client/__init__.py
+++ mcash/mapi_client/__init__.py
@@ -5,4 +5,3 @@ from mcash.mapi_client.mapi_error import *
 from mcash.mapi_client.mapi_response import *
 from mcash.mapi_client.backends import *
 from mcash.mapi_client.backends.requestsframework import *
-from mcash.mapi_client.backends.urlfetch import *
