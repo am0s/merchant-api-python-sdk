894532a7001fbda22d5eb9ed764c3eb3be86094b
Jan Borsodi <jb@aplia.no>
2017-01-12 13:31:05 +0100

    Several fixes to authentication with regards to Python 3 and requests
    - Use binary prefixes for all strings and encode strings to binary
      before concating.
    - The requests callback now uses only one parameter which is the
      request object.
      Updated the code for auth to use that and added a second method for
      transforming method, url, headers and body before it is sent to
      requests.
    - Decode the response content from utf-8 to str if needed (Python 3).

--- mcash/mapi_client/auth.py
+++ mcash/mapi_client/auth.py
@@ -6,64 +6,78 @@ from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
+from .six import binary_type
+
 __all__ = ["OpenAuth", "SecretAuth", "RsaSha256Auth"]
 
 
-class OpenAuth(object):
-    """Attaches no authentication to the given Request object."""
+class AuthBase(object):
+    """Base class for authentation classes"""
 
-    def __call__(self, method, url, headers, body):
+    def __call__(self, req):
+        return req
+
+    def prepare_request(self, method, url, headers, body):
         return method, url, headers, body
 
 
-class SecretAuth(object):
+class OpenAuth(AuthBase):
+    """Attaches no authentication to the given Request object."""
+
+    def __call__(self, req):
+        return req
+
+
+class SecretAuth(AuthBase):
     """Attaches Authentication with secret token to
     the given Request object."""
 
     def __init__(self, secret):
         self.secret = secret
 
-    def __call__(self, method, url, headers, body):
-        headers['Authorization'] = 'SECRET ' + self.secret
-        return method, url, headers, body
+    def __call__(self, req):
+        req.headers['Authorization'] = 'SECRET ' + self.secret
+        return req
 
 
-class RsaSha256Auth(object):
+class RsaSha256Auth(AuthBase):
     """Attaches RSA authentication to the given Request object."""
 
     def __init__(self, privkey):
         self.signer = PKCS1_v1_5.new(RSA.importKey(privkey))
 
-    def __call__(self, method, url, headers, body):
-        headers['X-Mcash-Timestamp'] = self._get_timestamp()
-        headers['X-Mcash-Content-Digest'] = self._get_sha256_digest(body)
-        headers['Authorization'] = self._sha256_sign(method, url, headers, body)
-        return method, url, headers, body
+    def __call__(self, req):
+        req.headers['X-Mcash-Timestamp'] = self._get_timestamp()
+        req.headers['X-Mcash-Content-Digest'] = self._get_sha256_digest(req.body or b'')
+        req.headers['Authorization'] = self._sha256_sign(req.method, req.url, req.headers, req.body or b'')
+        return req
 
     def _get_timestamp(self):
         """Return the timestamp formatted to comply with
         Merchant API expectations.
         """
-        return str(strftime("%Y-%m-%d %H:%M:%S"))
+        return strftime("%Y-%m-%d %H:%M:%S").encode('utf-8')
 
     def _get_sha256_digest(self, content):
         """Return the sha256 digest of the content in the
         header format the Merchant API expects.
         """
         content_sha256 = base64.b64encode(SHA256.new(content).digest())
-        return 'SHA256=' + content_sha256
+        return b'SHA256=' + content_sha256
 
     def _sha256_sign(self, method, url, headers, body):
         """Sign the request with SHA256.
         """
-        d = ''
-        sign_headers = method.upper() + '|' + url + '|'
+        d = b''
+        sign_headers = method.upper().encode('utf-8') + b'|' + url.encode('utf-8') + b'|'
         for key, value in sorted(headers.items()):
             if key.startswith('X-Mcash-'):
-                sign_headers += d + key.upper() + '=' + value
-                d = '&'
+                if not isinstance(value, binary_type):
+                    value = value.encode('utf-8')
+                sign_headers += d + key.upper().encode('utf-8') + b'=' + value
+                d = b'&'
 
         rsa_signature = base64.b64encode(
             self.signer.sign(SHA256.new(sign_headers)))
 
-        return 'RSA-SHA256 ' + rsa_signature
+        return b'RSA-SHA256 ' + rsa_signature
--- mcash/mapi_client/backends/requestsframework.py
+++ mcash/mapi_client/backends/requestsframework.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 from ..mapi_response import MapiResponse
 
+from ..six import binary_type
+
 __all__ = ["RequestsFramework"]
 
 
 class RequestsFramework(object):
     def dispatch_request(self, method, url, body, headers, auth, files=None):
+        """
+        :type auth: mcash.mapi_client.auth.AuthBase
+        """
         import requests
-        method, url, headers, data = auth(method, url, headers, body)
+        method, url, headers, body = auth.prepare_request(method, url, headers, body)
         res = requests.request(method,
                                url,
-                               data=data,
+                               data=body,
                                headers=headers,
                                auth=auth,
-                               timeout=60,
-                               files=files)
-        return MapiResponse(res.status_code, res.headers, res.content)
+                               timeout=60)
+        content = res.content
+        if isinstance(res.content, binary_type):
+            content = res.content.decode('utf-8')
+        return MapiResponse(res.status_code, res.headers, content)
